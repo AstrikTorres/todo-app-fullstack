60784a2a6139702ae321e0f36ee21de97ae02b26
Astrik Torres <astriktorres@gmail.com>
2022-06-05 18:28:04 -0500

    Entidad Todos creada

---
+++ src/main/java/com/astrik/todoappapi/entity/ToDo.java
@@ -0,0 +1,46 @@
+package com.astrik.todoappapi.entity;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "todos")
+public class ToDo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    @Column
+    private Boolean completed;
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
