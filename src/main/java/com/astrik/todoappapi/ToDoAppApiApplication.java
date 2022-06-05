b2c0f2fb56034880da559d46f76772e6461e8fa1
Astrik Torres <astriktorres@gmail.com>
2022-06-05 15:01:13 -0500

    First commit

---
+++ src/main/java/com/astrik/todoappapi/ToDoAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.astrik.todoappapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoAppApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToDoAppApiApplication.class, args);
+    }
+
+}
